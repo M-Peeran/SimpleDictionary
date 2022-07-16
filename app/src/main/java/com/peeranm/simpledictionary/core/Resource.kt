32becb2625dd689fb2ed00b07970aab858a307ab
M-Peeran <peeramakrabbi@gmail.com>
2022-07-16 14:13:22 +0530

    Deleted unwanted class, refactored Resource class

--- app/src/main/java/com/peeranm/simpledictionary/core/Resource.kt
+++ app/src/main/java/com/peeranm/simpledictionary/core/Resource.kt
@@ -1,8 +1,9 @@
 package com.peeranm.simpledictionary.core
 
-sealed class Resource<T>(data: T? = null, message: String? = null) {
-    class Loading<T>(val data: T? = null) : Resource<T>(data)
-    class Success<T>(val data: T) : Resource<T>(data)
-    class Error<T>(val data: T? = null, val message: String) : Resource<T>(data, message)
+
+sealed class Resource<T> {
+    class Loading<T>(val data: T? = null) : Resource<T>()
+    class Success<T>(val data: T) : Resource<T>()
+    class Error<T>(val data: T? = null, val message: String) : Resource<T>()
     class None<T> : Resource<T>()
 }
